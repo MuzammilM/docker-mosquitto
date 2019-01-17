940a350a29e2f8a098ff27580ecd578dd60dec66
Muzammil Hasan Momin <muzammilmomin@live.com>
2019-01-17 16:32:30 +0530

    Update mosquitto_setup.sh

--- mosquitto_setup.sh
+++ mosquitto_setup.sh
@@ -10,5 +10,4 @@ echo "Creating mosquitto password file for user "$USERNAME
 touch mosquitto/config/passwd && sudo chown 1883:1883 mosquitto/config/passwd && docker run -it -v ${PWD}/mosquitto/config/passwd:/passwd eclipse-mosquitto:1.5 mosquitto_passwd -b passwd $USERNAME
 echo "Creating default mosquitto configuration file"
 docker run -it eclipse-mosquitto:1.5 cat /mosquitto/config/mosquitto.conf > mosquitto/config/mosquitto.conf
-chmod 644 mosquitto/config/password
-docker run -d -it -p 1883:1883 -p 9001:9001 -v ${PWD}/mosquitto/config/mosquitto.conf:/mosquitto/config/mosquitto.conf -v ${PWD}/mosquitto/config/password:/mosquitto/config/password -v ${PWD}/mosquitto/data:/mosquitto/data -v ${PWD}/mosquitto/log:/mosquitto/log eclipse-mosquitto:1.5
+docker run -d -it -p 1883:1883 -p 9001:9001 -v ${PWD}/mosquitto/config/mosquitto.conf:/mosquitto/config/mosquitto.conf -v ${PWD}/mosquitto/config/passwd:/mosquitto/config/password -v ${PWD}/mosquitto/data:/mosquitto/data -v ${PWD}/mosquitto/log:/mosquitto/log eclipse-mosquitto:1.5
