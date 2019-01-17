42a4636dc167e652e390b49908c1ca06a6e3ff1b
Muzammil Hasan Momin <muzammilmomin@live.com>
2019-01-17 21:49:37 +0530

    Update mosquitto_setup.sh

--- mosquitto_setup.sh
+++ mosquitto_setup.sh
@@ -1,6 +1,12 @@
 # Create a mosquitto instance with authentication enabled.
 # Update mosquitto config within mosquitto/config/mosquitto.conf and restart docker container.
 # Author: MuzammilM
+ERROR="\033[0;31m"
+WARN="\033[0;33m"
+SUCCESS="\033[0;32m"
+DEFAULT="\033[0;37m"
+reset=`tput sgr0`
+
 read -rp"Enter username: " -e USERNAME
 sudo ufw allow 1883
 sudo ufw allow 9001
@@ -8,7 +14,7 @@ sudo ufw allow 22
 sudo ufw --force enable
 mkdir -p mosquitto/config mosquitto/log mosquitto/data
 chown -R 1883:1883 mosquitto
-echo "Creating mosquitto password file for user "$USERNAME
+echo -e "${SUCCESS}Creating mosquitto password file for user ${reset}"$USERNAME
 touch mosquitto/config/passwd && sudo chown 1883:1883 mosquitto/config/passwd && docker run -it -v ${PWD}/mosquitto/config/passwd:/passwd eclipse-mosquitto:1.5 mosquitto_passwd -c passwd $USERNAME
 echo "Creating default mosquitto configuration file"
 docker run -it eclipse-mosquitto:1.5 cat /mosquitto/config/mosquitto.conf > mosquitto/config/mosquitto.conf
